96a13184fdf91dc4634a1b0d3462be5032b54e39
Kian <endud5118@gmail.com>
2020-01-06 00:18:08 +1100

    halfWayOfchatApp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Join from './components/join/Join'
+import Chat from './components/chat/Chat'
 
-const App = () => {
-   return (
-      <div>
-         
-      </div>
-   )
-}
-
+const App = () => (
+<Router>
+   <Route path='/' exact component={Join} />
+   <Route path='/chat' exact component={Chat} />
+   
+</Router>)
 export default App
