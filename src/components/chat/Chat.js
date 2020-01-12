8b88462314fb821ae73f3b514e7611638c675b0d
Kian <endud5118@gmail.com>
2020-01-12 12:22:28 +1100

    fixed

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = ' https://customzing-color-chat-app.herokuapp.com/';
+  const ENDPOINT ='https://customzing-color-chat-app.herokuapp.com/';
 
 
   useEffect(() => {
